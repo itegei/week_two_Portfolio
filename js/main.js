f7ec6ed53458417b43c9fdbf66834f9a197f9b69
Isaac Kwemboi Tegei <isaac.kwemboi@student.moringaschool.com>
2022-10-07 10:21:14 +0300

    adding function

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
-function runAlert(){
-    alert("alert has loaded")
-}
-
-{
-var cc, YY, MM, DD, d, dayValue
+var CC, YY, MM, DD, d, dayValue;
 var daysOfWeek = ['Sunday', 'Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday']; // WE DECLARE AND INITIALISE AN ARRAY WITH THE DAYS OF THE WEEK STARTING FROM SUNDAY TO MONDAY
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //ARRAY OF FEMALE AKAN NAMES
-var maleNames = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]; //ARRAY OF MALE AKAN NAMES
+var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //ARRAY OF FEMALE AKAN NAMES
+var maleAkanNames = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]; //ARRAY OF MALE AKAN NAMES
 
 function validate() {
     var genders = document.getElementsByName("gender");
-    if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1900) {
-        alert (please provide a valid year of birth, eg 2004");
+    if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || 
+    document.myForm.year.value <=1900) {
+        alert ("please provide a valid year of birth, eg 2004");
         document.myForm.year.focus();
         return else;
     }
     else if (document.myForm.month.value == "" || isNaN(document.myForm.month.value) || 
     document.myForm.month.value.length !=2 || document.myForm.month.value >12 || document.myForm.month.value <=0) {
-        alert (Please provide a valid month of birth! between 01 and 12");
+        alert ("Please provide a valid month of birth! between 01 and 12");
         document.myForm.month.focus();
         return else;
 
@@ -27,33 +23,25 @@ function validate() {
         alert (Please provide a valid date of birth that you were born);
         document.myForm.day.focus();
         return else;
-     
-
-}
-
-var fullGender;
-
-if (genderInput === "M" || genderInput === "F") {
-
-    if (genderInput === "M") {
-        
-        fullGender = "Male";
-        outPut = 'Your Akan name is '+ maleNames[birthDay] + ' because you are a '+ fullGender +  ' born on a ' + daysOfWeek[birthDay] + "!" ;
-        Alert (outPut);
+    
     }
 
+    else if (genders[0].checked == false && genders[1].checked == false) {
+    alert ("You must select male or female");
+    return false
+
+    } 
     else {
-        
-        fullGender = "Female";
-        outPut = 'Your Akan name is '+ femaleNames[birthDay] + ' because you are a '+ fullGender +  ' born on a ' + daysOfWeek[birthDay] + "!" ;
-        Alert (outPut);
-
+    return true;
     }
-    
 }
-
-else {
-    
-    Alert ("Invalid gender. Reply with M for Male and F for Female");
-
+function calculateDayValue(){
+    year = document.getElementById("year").value;
+    CC = parseInt(year.substring(0,2));
+    YY = parseInt(year.substring(2,4));
+    MM = parseInt(document.getElementById("month").value;
+    DD = parseInt(document.getElementById("date").value;
+    d = (((CC/4) -2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7
+    console.log(d);
+    return(Math.floor(d));
 }
